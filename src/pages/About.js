4228f163d42d019b3cb066f19f54da47f796f137
garciaedy <edygarcia@macbook-pro.attlocal.net>
2020-08-11 20:38:26 -0400

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,9 +3,22 @@ import React, { Component } from 'react'
 export default class componentName extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <section className="section about-section" >
+        <h1 className="section-title" > About us</h1>
+        <p>
+        I'm baby shabby chic +1 selvage franzen, art party tilde
+         fanny pack YOLO tumblr mumblecore messenger bag tote bag 
+         waistcoat shaman. Chartreuse salvia small batch chambray,
+          marfa VHS twee freegan +1 succulents flannel trust fund 
+          typewriter. Semiotics pinterest umami, keytar single-origin 
+          coffee drinking vinegar readymade kitsch kickstarter small batch 
+          portland cloud bread. Mlkshk truffaut scenester tote bag, bushwick
+           woke vexillologist tumeric chia actually hell of.
+
+
+
+        </p>
+      </section>
     )
   }
 }
