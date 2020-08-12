ed7bc7d0a60c733e009378f70ed2398ee6af2b31
garciaedy <edygarcia@macbook-pro.attlocal.net>
2020-08-11 22:57:53 -0400

    testing functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,9 +11,17 @@ export default function Home() {
     const [cocktails, setCocktails] = React.useState([]);
     
     React.useEffect(() =>{
-      fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`).then(response => response.json())
-      .then(data => setCocktails(data.drinks));
-      
+      async function getDrinks(){
+try {
+  const response =await  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=
+  ${searchTerm}`).then(response => response.json())
+  .then(data => setCocktails(data.drinks));
+} catch (error) {
+  
+}
+     
+      }
+
     },[searchTerm]);
 
     return (
