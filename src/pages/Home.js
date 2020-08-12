60dd77ef0fb76f88c3494bcc1f8a4231ab690573
garciaedy <edygarcia@macbook-pro.attlocal.net>
2020-08-11 22:02:15 -0400

    modified and added API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
+import React  from 'react';
+import CocktailsList from '../components/CocktailList'
+import SearchForm from '../components/SearchForm'
 
-export default class componentName extends Component {
-  render() {
+
+
+export default function Home() {
+
+  const [loading, setLoading] = React.useState(false);
+    const [searchTerm, setSearchTerm] = React.useState('a');
+    const [cocktails, setCocktails] = React.useState([]);
+    
+    React.useEffect(() =>{
+      fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita').then(response => response.json())
+      .then(data => setCocktails(data.drinks));
+      
+    },[]);
+
     return (
-      <div>
-        
-      </div>
-    )
+      <main>
+        <SearchForm setSearchTerm={setSearchTerm}/>
+        <CocktailsList loading={loading} cocktails={cocktails}/>
+      </main>
+    );
+
   }
-}
