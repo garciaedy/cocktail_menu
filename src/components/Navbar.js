dff703fc08f459508d292cbafb92fd66b74c1f28
garciaedy <edygarcia@macbook-pro.attlocal.net>
2020-08-11 20:33:17 -0400

    added links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { Component } from 'react'
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
 
-export default function Navbar() {
-  return <h1>navbar component</h1>;
+export default class componentName extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        
+      <div className="nav-center">
+        <img src={logo} alt="cocktail db logo" className="logo"/>
+        <ul className="nav-links">
+        <li> 
+          <Link to="/" > Home</Link>
+        </li>
+        <li> 
+          <Link to="/about" > about</Link>
+        </li>
+        </ul>
+      </div>
+
+      </nav>
+    )
+  }
 }
