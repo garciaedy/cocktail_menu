ac49df6be0290f42a8d6abe57755160fd104d412
garciaedy <edygarcia@macbook-pro.attlocal.net>
2020-08-11 22:48:14 -0400

    added searchTerm aguement

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,10 +11,10 @@ export default function Home() {
     const [cocktails, setCocktails] = React.useState([]);
     
     React.useEffect(() =>{
-      fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita').then(response => response.json())
+      fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`).then(response => response.json())
       .then(data => setCocktails(data.drinks));
       
-    },[]);
+    },[searchTerm]);
 
     return (
       <main>
