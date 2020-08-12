0df741c1854ae6776abf6039853600a6e4e35555
garciaedy <edygarcia@macbook-pro.attlocal.net>
2020-08-11 20:58:39 -0400

    created error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
 export default class componentName extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <section className="error-page section">
+        <div className="error-container">
+          <h1> Oops! it's a dead end!</h1>
+          <Link to="/" className="btn btn-primary">  Back Home</Link>
+        </div>
+      </section>
     )
   }
 }
