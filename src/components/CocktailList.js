d97078a8d6a62610eb1abcf608efe33693186428
garciaedy <edygarcia@macbook-pro.attlocal.net>
2020-08-12 20:39:54 -0400

    setting up cocktailList component

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react'
+import React from "react"
+import Cocktail from './Cocktail';
 
-export default class componentName extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
+export default function CocktailList({cocktails, loading}){
+  if (loading) {
+    return <h2 className="section-title">Loading...</h2>
   }
+  if(cocktails.letgh)
 }
